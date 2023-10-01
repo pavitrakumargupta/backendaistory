0afb9dedbec3ceaee9de0e8d679ace4b2589df3e
pavitrakumargupta <pavitragupta021@gmail.com>
2023-10-01 15:52:47 +0530

    usdfy

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express=require("express")
 const cors =require("cors")
-const userRoutes =require("./routes/routes.js")
 const mongoose = require('mongoose');
 const app=express()
 require("dotenv").config();
@@ -21,7 +20,40 @@ mongoose
 app.use(cors())   
 app.use(express.json()) 
  
-app.use("/",userRoutes)
+app.post("/api/setStories",async (req, res, next) => {
+  console.log(req.body);
+    try {
+        // Extract the story data from the request body
+        const { story, prompt, likes, dislikes } = req.body;
+
+        const newStory = new Story({
+            story: story,
+            prompt: prompt,
+            likes: likes,
+            dislikes: dislikes,
+        });
+        console.log(newStory);
+        await newStory.save();
+
+        res.status(200).json({ message: 'Story saved successfully' });
+    } catch (error) {
+        console.error(error);
+        console.log(error);
+        res.status(500).json({ message: 'Error saving the story' });
+    }
+})
+
+app.get("/api/getStories",async (req, res, next) => {
+  try {
+    // Use Mongoose to query the database for stories
+    const stories = await Story.find({});
+
+    res.status(200).json(stories);
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching stories' });
+}
+})
 
 
 app.get('/', function(req, res){
